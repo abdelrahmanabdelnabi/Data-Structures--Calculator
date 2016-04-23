bf164e95cc1caed6dda5581a18b4e9c6a238fc89
abdulrahman <abdulrahman@192.168.56.1>
2016-04-23 22:03:02 +0200

    initial commit, created a source file for the hash table

---
+++ HashTable.c
@@ -0,0 +1,11 @@
+/*
+ * HashTable.c
+ *
+ *  Created on: 23 Apr 2016
+ *      Author: abdulrahman
+ */
+
+/*
+ * implements the Hash Table data structure to store variable names (key) and
+ * their value (value)
+ */
